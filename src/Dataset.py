cece65217adab4b4e10fbfb14d3d4dc9b35d26b8
fvpfvpfvp <47208626+fvpfvpfvp@users.noreply.github.com>
2024-04-24 17:45:55 +0200

    Implemented Dataset.py

--- src/Dataset.py
+++ src/Dataset.py
@@ -1,11 +1,29 @@
+import torch
 from torchvision import datasets, transforms
 
 class Dataset:
+
     def __init__(self, datasetName, batchSize):
-        pass
 
-    def loadTrainBatch(self):
-        pass
+        # Todo: make generic
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 
-    def loadTestBatch(self):
-        pass
+        self.train = getattr(datasets, datasetName)('datasets', train=True, download=True, transform=transform)
+        self.test = getattr(datasets, datasetName)('datasets', train=False, transform=transform)
+
+        self.trainLoader = torch.utils.data.DataLoader(self.train, batch_size=batchSize)
+        self.testLoader = torch.utils.data.DataLoader(self.test, batch_size=batchSize)
+
+        self.batchSize = batchSize
+
+    def trainingEnumeration(self):
+        return self.trainLoader
+
+    def testingEnumeration(self):
+        return self.testLoader
+
+    def trainBatchCount(self):
+        return len(self.trainLoader.datasets)
+
+    def testBatchCount(self):
+        return len(self.testLoader.datasets)
