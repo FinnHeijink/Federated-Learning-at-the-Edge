7eb2b150fd2f662b8502ca515763f2c8c5256110
Nassim26 <Nassimbeladel026@gmail.com>
2024-06-10 18:16:38 +0200

    Add files via upload

--- Hardware/CNN/filter_fsm.sv
+++ Hardware/CNN/filter_fsm.sv
@@ -1,5 +1,5 @@
 module filter_fsm #(
-  parameter width = 16,
+  parameter width = 12,
   parameter input_width = 8,
   parameter im_dim = 28,
   parameter k_size = 9, // 3x3
@@ -15,16 +15,18 @@ module filter_fsm #(
   output logic           [7:0]  pixel_o,
   
   output logic                  conv_finished,
+    
+  output logic					finished_image,
   
-  input logic [143:0] k_val
+  input logic [107:0] k_val
 );
 
   typedef enum {
       init, 
-      commence_construction,
-      construct_kernel, 
       await_buffers,
+	  preprocess,
       process, 
+	  stall_conv,
       done
   } state_t; 
     
@@ -33,19 +35,19 @@ module filter_fsm #(
   typedef enum {
     idle, 
     convolve,
+	stall_reading,
     finished  
   } read_state;
 
   read_state r_state_d, r_state_q;
   
   // logic [2:0][2:0][input_width-1:0] pixel_i;
-  logic [2:0][2:0][width-1:0] kernel_q, kernel_d, k_out; // 3x3 kernel in registers, 16-bit resolution
   logic [2:0][2:0][input_width-1:0] pixel_grid;
-  logic commence, k_constructed, r_en;
-  
-  logic [$clog2(k_size)-1:0] k_address;
+    
+  logic [1:0] conv_stall; 
   
   logic conv_data_valid;
+  logic [1:0] increment_buffer;
   
   logic [$clog2(im_dim)-1:0] row_pix_counter; // 5 bit signal
   logic [$clog2(im_dim*im_dim)-1:0] pix_counter; // 10 bit signal (28^2)
@@ -53,25 +55,24 @@ module filter_fsm #(
   logic [1:0] active_write_buffer;
   logic [1:0] active_read_buffer;
   
-  logic p1_read, p2_read, p3_read, p4_read, p1_valid, p2_valid, p3_valid, p4_valid;
+  
+  logic p1_valid, p2_valid, p3_valid, p4_valid;
   logic read_buffer;
   logic [3:0] buff_valid;
   logic [3:0] buff_read;
-  logic [8:0] read_counter;
+  logic [4:0] read_counter;
   
   logic [2:0][input_width-1:0] p1_buffer, p2_buffer, p3_buffer, p4_buffer;
   
   logic [$clog2(im_dim)-1:0] read_row_counter; // Tracks total number of read rows committed to buffers, to know when an image is entirely read!
-  
+    
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (~rst_ni) begin
       state_q <= init;  
       r_state_q <= idle; 
-      kernel_q <= '0; 
     end else begin
       state_q <= state_d;
       r_state_q <= r_state_d;
-      kernel_q <= kernel_d; 
     end 
   end
   
@@ -79,18 +80,31 @@ module filter_fsm #(
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (~rst_ni) begin
       row_pix_counter <= '0;  
-    end else if (pix_data_valid && row_pix_counter == 'd27) begin
+    end else if (pix_data_valid && row_pix_counter == im_dim-1) begin
       row_pix_counter <= '0;
     end else if (pix_data_valid) begin
-      row_pix_counter <= row_pix_counter + 1;
-    end 
+      row_pix_counter <= row_pix_counter + 1'b1;
+	  if (conv_stall > 'd0) begin
+	    conv_stall <= conv_stall - 1'b1;
+		increment_buffer <= increment_buffer + 1'b1;
+	  end 
+    end else if (!pix_data_valid && conv_stall > 'd0) begin
+	  conv_stall <= conv_stall - 1'b1;
+	  increment_buffer <= increment_buffer + 1'b1;
+	end 
   end
+  
+  always_ff @(posedge clk_i) begin 
+    if (increment_buffer == 2'b10) begin
+	  increment_buffer <= 'd0;
+	end 
+  end 
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (~rst_ni) begin
       pix_counter <= '0;  
     end else if (pix_data_valid) begin
-      pix_counter <= pix_counter + 1;
+      pix_counter <= pix_counter + 1'b1;
     end 
   end
 
@@ -98,14 +112,14 @@ module filter_fsm #(
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (~rst_ni) begin
       active_write_buffer <= '0;  // start with p1
-    end else if (row_pix_counter == 'd27 && pix_data_valid) begin
-      active_write_buffer <= active_write_buffer + 1; // exploit overflow to loop back to 0!
+    end else if (row_pix_counter == im_dim-1 && pix_data_valid) begin
+      active_write_buffer <= active_write_buffer + 1'b1; // exploit overflow to loop back to 0!
     end
   end
 
   always_comb begin
     buff_valid = '0;
-    buff_valid[active_write_buffer] = 1'b1; // Validity signal determines which row buffer stores input pixel
+    buff_valid[active_write_buffer] = pix_data_valid; // Validity signal determines which row buffer stores input pixel
 	{p4_valid, p3_valid, p2_valid, p1_valid} = buff_valid;
   end
 
@@ -113,18 +127,23 @@ module filter_fsm #(
     if (~rst_ni) begin
       read_counter <= '0;  
       read_row_counter <= '0;
-    end else if (read_buffer && read_counter == 'd25) begin
+	  conv_stall <= '0;
+	  increment_buffer <= '0;
+    end else if (read_buffer && read_counter == im_dim-3 && conv_stall == 'd0) begin
       read_counter <= '0;
-      read_row_counter <= read_row_counter + 1; 
-    end else if (read_buffer) begin
-      read_counter <= read_counter + 1; // counts every row during active convolution phase, similar to row_pix_counter
+      read_row_counter <= read_row_counter + 1'b1; 
+	  conv_stall <= 'd2;
+	  increment_buffer <= 2'b01;
+    end else if (read_buffer && conv_stall == 'd0) begin
+      read_counter <= read_counter + 1'b1; // counts every row during active convolution phase, similar to row_pix_counter
     end
   end
+  
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (~rst_ni) begin
       active_read_buffer <= '0;
-    end else if (read_counter == 'd25 && read_buffer) begin
+    end else if (increment_buffer == 2'b10) begin
       active_read_buffer <= active_read_buffer + 1;  
     end
   end
@@ -133,28 +152,18 @@ module filter_fsm #(
     case(active_read_buffer)
       2'b00: begin
         pixel_grid = {p1_buffer, p2_buffer, p3_buffer};
-        buff_read = {{3{read_buffer}}, 1'b0}; // 4th buffer being constructed, we read the first 3 buffers
-		{p1_read, p2_read, p3_read, p4_read} = buff_read;
       end
       2'b01: begin
         pixel_grid = {p2_buffer, p3_buffer, p4_buffer};
-        buff_read = {1'b0, {3{read_buffer}}}; // We shift the active buffers every row, read-write duality!!!
-		{p1_read, p2_read, p3_read, p4_read} = buff_read;
       end
       2'b10: begin
         pixel_grid = {p3_buffer, p4_buffer, p1_buffer};
-        buff_read = {read_buffer, 1'b0, {2{read_buffer}}}; // this is cool, 2-state FSM controls it :D
-		{p1_read, p2_read, p3_read, p4_read} = buff_read;
       end
       2'b11: begin
-        pixel_grid = {p4_buffer, p1_buffer, p1_buffer};
-        buff_read = {{2{read_buffer}}, 1'b0, read_buffer};
-		{p1_read, p2_read, p3_read, p4_read} = buff_read;
+        pixel_grid = {p4_buffer, p1_buffer, p2_buffer};
       end
       default: begin
         pixel_grid = '0;
-        buff_read = '0;
-		{p1_read, p2_read, p3_read, p4_read} = buff_read;
       end
     endcase
   end
@@ -163,7 +172,7 @@ module filter_fsm #(
     case(r_state_q) // @suppress "Default clause missing from case statement"
       idle: begin
 	    read_buffer <= 1'b0;
-        if (pix_counter >= 83) begin // active_write_buffer ought to be at state 2'b11
+        if (pix_counter >= (3*im_dim-1)) begin // active_write_buffer ought to be at state 2'b11
           r_state_d <= convolve; // First 3 lines have been read 
         end else begin
           r_state_d <= idle;
@@ -172,12 +181,23 @@ module filter_fsm #(
       
       convolve: begin
         read_buffer <= 1'b1; 
-        if (read_row_counter >= 'd26) begin
+        if (read_row_counter >= im_dim-2) begin
           r_state_d <= finished;
-        end else begin 
+        end else if (conv_stall > 'd0) begin 
+		  r_state_d <= stall_reading;
+		end else begin 
           r_state_d <= convolve;
         end
       end
+	  
+	  stall_reading: begin 
+	    read_buffer <= 1'b0;
+		if (conv_stall == 'd0) begin 
+		  r_state_d <= convolve;
+		end else begin 
+		  r_state_d <= stall_reading;
+		end 
+	  end
       
       finished: begin
         read_buffer <= 1'b0;
@@ -189,56 +209,56 @@ module filter_fsm #(
   always_comb begin
     case(state_q)
       init: begin
-        kernel_d <= kernel_q;
-        commence <= 1'b0; 
         conv_data_valid <= 1'b0;
-        state_d <= commence_construction; 
+		finished_image <= 1'b0; 
+        state_d <= await_buffers; 
       end
-      
-      commence_construction: begin
-        kernel_d <= kernel_q;
-        commence <= 1'b1; 
-        conv_data_valid <= 1'b0;
-        state_d <= construct_kernel; 
-      end
-      
-      construct_kernel: begin // Does BRAM-reader FSM provide kernel in correct form? [2:0][2:0][15:0]
-        kernel_d <= k_out;     
-        commence <= 1'b0;
-        conv_data_valid <= 1'b0;
-        if (k_constructed == 1'b1) begin
-          state_d <= await_buffers;   
-        end else begin
-          state_d <= construct_kernel;
-        end
-      end
-      
+       
       await_buffers: begin
-        kernel_d <= kernel_q;
-        commence <= 1'b0;
         conv_data_valid <= 1'b0;
-        if (read_buffer == 1'b1) begin
+		finished_image <= 1'b0; 
+        if (pix_counter == 'd83) begin
           state_d <= process;
         end else begin
           state_d <= await_buffers;
         end
       end
+	  
+	  preprocess: begin
+        conv_data_valid <= 1'b0;
+		finished_image <= 1'b0; 
+		state_d <= process;
+	  end 
       
       process: begin
-        kernel_d <= kernel_q; 
-        commence <= 1'b0;    
-        conv_data_valid <= 1'b1;
+		finished_image <= 1'b0; 
         if (r_state_q == finished && conv_finished == 1'b1) begin
+		  conv_data_valid <= 1'b0;
           state_d <= done;
-        end else begin
+        end else if (conv_stall > 'd0) begin
+		  conv_data_valid <= 1'b0;
+		  state_d <= stall_conv;
+		end else begin
+		  conv_data_valid <= 1'b1;
           state_d <= process;
         end
       end
-      
+	  
+	  stall_conv: begin
+		conv_data_valid <= 1'b0; 
+		finished_image <= 1'b0; 
+		if (conv_stall == 'd0) begin
+		  state_d <= process;
+		end else if (r_state_q == finished && conv_finished == 1'b1) begin 
+		  state_d <= done;  
+		end else begin
+          state_d <= stall_conv;
+		end 
+	  end
+	  
       done: begin
-        kernel_d <= kernel_q;
-        commence <= 1'b0;
         conv_data_valid <= 1'b0;
+		finished_image <= 1'b1; 
         state_d <= done;
       end
     endcase
@@ -251,75 +271,62 @@ module filter_fsm #(
     // .switch_i(switch_i),
     .pixel_i(pixel_grid),
     .data_valid(conv_data_valid),
-    .k(kernel_q),
+    .k(k_val),
     .pixel_o(pixel_o),
     .output_valid(conv_finished)  
   );
-  
-/*
-  // Kernel BRAM
-  kernel_weight  #(
-    .width(width),
-    .k_size(k_size)
-  ) k_bram (
-    .clk_i(clk_i),
-    .rst_ni(rst_ni),
-    .r_en(r_en),
-    .address(k_address), 
-    .k_out(k_val)  
-  );
- */
-  
-  // Reads k_BRAM and returns kernel in proper form, takes 2 clk-cycles
-  kernel_constructor #(
-    .width(width),
-    .k_size(k_size)
-  ) k_const (
-      .clk_i(clk_i),
-      .rst_ni(rst_ni),
-      .commence(commence),
-      .r_en(r_en),
-      .k_val(k_val),
-      .k_address(k_address),
-      .kernel(k_out),
-      .finished(k_constructed)
-  );
-  
+ 
   // The following "pixel buffers" function as row buffers, buffering row-data for the kernel
   
-  pixel_buffer p1 (
+  pixel_buffer # (
+	.input_width(input_width),
+	.buff_size(3),
+	.im_dim(im_dim)
+  ) p1 (
     .clk_i(clk_i),
     .rst_ni(rst_ni),
     .pix_data(pixel_i),
     .data_valid(p1_valid),
-    .data_read(buff_read[0]),
+	.read_address(read_counter),
     .pix_buffer(p1_buffer)  
   );
   
-  pixel_buffer p2 (
+  pixel_buffer # (
+	.input_width(input_width),
+	.buff_size(3),
+	.im_dim(im_dim)
+  ) p2 (
     .clk_i(clk_i),
     .rst_ni(rst_ni),
     .pix_data(pixel_i),
     .data_valid(p2_valid),
-    .data_read(buff_read[1]),
+	.read_address(read_counter),
     .pix_buffer(p2_buffer)  
   );
   
-  pixel_buffer p3 (
+  pixel_buffer # (
+	.input_width(input_width),
+	.buff_size(3),
+	.im_dim(im_dim)
+  ) p3 (
     .clk_i(clk_i),
     .rst_ni(rst_ni),
     .pix_data(pixel_i),
     .data_valid(p3_valid),
-    .data_read(buff_read[2]),
+	.read_address(read_counter),
     .pix_buffer(p3_buffer)
   );
   
-  pixel_buffer p4 (
+  pixel_buffer # (
+	.input_width(input_width),
+	.buff_size(3),
+	.im_dim(im_dim)
+  ) p4 (
     .clk_i(clk_i),
     .rst_ni(rst_ni),
     .pix_data(pixel_i),
     .data_valid(p4_valid),
-    .data_read(buff_read[3]),
+	.read_address(read_counter),
     .pix_buffer(p4_buffer)  
   );
     
